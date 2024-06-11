17379963c62b8960ca91dc65415188d16bafbcf7
paralol <santinico36@hotmail.com>
2024-06-11 12:36:12 -0500

    asd

--- src/main/java/edu/escuelaing/arsw/ASE/app/App.java
+++ src/main/java/edu/escuelaing/arsw/ASE/app/App.java
@@ -1,19 +1,13 @@
 package edu.escuelaing.arsw.ASE.app;
 
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public String[] loc(String name){
-        return null;
-    }
 
-    public String[] phy(String name){
-        return null;
-    }
-    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
